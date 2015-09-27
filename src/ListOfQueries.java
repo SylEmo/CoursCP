38890b2040589d7677d5363a213d601925ca770e
Thomasgitt <thomas.gittinger@live.fr>
2015-09-28 00:28:52 +0200

    Requêtes sql

--- src/ListOfQueries.java
+++ src/ListOfQueries.java
@@ -8,9 +8,9 @@
  */
 public class ListOfQueries {
 	
-	public static String QUERY_ADD_ATELIER = "";
-	public static String QUERY_DELETE_ATELIER = "";
-	public static String QUERY_LIST_ATELIER = "";
-	public static String QUERY_MODIFY_ATELIER = "";
-	public static String QUERY_GET_DETAILS_ATELIER = "";
+	public static String QUERY_ADD_ATELIER = "INSERT INTO atelier (Titre, Theme, Laboratoire, Duree, Capacite) VALUES ( ?, ?, ?, ?, ?)";
+	public static String QUERY_DELETE_ATELIER = "DELETE FROM atelier WHERE Id = ? ";
+	public static String QUERY_LIST_ATELIER = "SELECT Titre, Laboratoire FROM atelier";
+	public static String QUERY_MODIFY_ATELIER = "UPDATE atelier SET Theme=?, Laboratoire=?, Duree=?, Capacite=? WHERE Id=? ";
+	public static String QUERY_GET_DETAILS_ATELIER = "SELECT * FROM atelier WHERE Id=? ";
 }
