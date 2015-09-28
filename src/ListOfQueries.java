b9a5dedf921f6e5f933660b7f7c4c61efa6e50b5
Thomasgitt <thomas.gittinger@live.fr>
2015-09-28 18:16:19 +0200

    Modif requête accès détails atelier

--- src/ListOfQueries.java
+++ src/ListOfQueries.java
@@ -12,5 +12,5 @@ public class ListOfQueries {
 	public static String QUERY_DELETE_ATELIER = "DELETE FROM atelier WHERE Id = ? ";
 	public static String QUERY_LIST_ATELIER = "SELECT Titre, Laboratoire FROM atelier";
 	public static String QUERY_MODIFY_ATELIER = "UPDATE atelier SET Theme=?, Laboratoire=?, Duree=?, Capacite=? WHERE Id=? ";
-	public static String QUERY_GET_DETAILS_ATELIER = "SELECT * FROM atelier WHERE Id=? ";
+	public static String QUERY_GET_DETAILS_ATELIER = "SELECT * FROM atelier WHERE Id=? AND (SELECT * FROM laboratoire WHERE laboratoire.Id = atelier.Laboratoire) AND (SELECT * FROM horaire where horaire.IdAtelier=atelier.Id)";
 }
