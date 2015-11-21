b52499bcfefc39db47fe8ccf58557b88c6669947
Thomasgitt <thomas.gittinger@live.fr>
2015-11-21 16:50:37 +0100

    lien détails projet/liste sprints + recup liste

---
+++ WebApp/php/ListerSprints.php
@@ -0,0 +1,20 @@
+<?php
+$idprojet = $_POST['id'];
+//echo "testderetourphp".$bla;
+
+require_once("Verifier1.php");
+session_start();
+require_once("Connexion.php");
+$req= "select * from SPRINT WHERE IDPROJET=".$idprojet;
+$rs=mysql_query($req) or die(mysql_error());
+
+$result='';
+while($pr=mysql_fetch_assoc($rs)) {
+	$result.= $pr['ID'].'-'.$pr['NUMERO'].'&';
+}
+
+//on efface le dernier "-" pour aider le split dans le fichier liste-projets.html et ainsi empecher d'avoir une entrée vie dans le tableau et donc une ligne de projet vide
+$result = substr($result, 0, -1);
+
+echo $result;
+?>
