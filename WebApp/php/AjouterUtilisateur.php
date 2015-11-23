95b0ae6e9aceb2f9cc4590d138573867364612ac
Thomasgitt <thomas.gittinger@live.fr>
2015-11-23 17:24:32 +0100

    S1 - T1 Dette technique modification de la page d'inscription

--- WebApp/php/AjouterUtilisateur.php
+++ WebApp/php/AjouterUtilisateur.php
@@ -7,15 +7,9 @@ $pass1=$_POST['pwd'];
 $pass2=$_POST['pwd2'];
 $email=$_POST['email'];
 
-
-if($pass1==$pass2) {
     $pc=md5($pass1);
     $req = "insert into UTILISATEUR (IDENTIFIANT,EMAIL, MOT_DE_PASSE) VALUE ('$log','$email','$pc')";
     mysql_query($req) or die(mysql_error());
     header("location:../index.html");
-}
-else{
 
-    header("location:../ajouter-utilisateur.html");
-}
 ?>
