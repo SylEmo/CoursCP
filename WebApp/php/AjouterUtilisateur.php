7210cfffab149bb0d3d4153e233e8e22b584dbb9
Thomasgitt <thomas.gittinger@live.fr>
2015-11-25 00:08:39 +0100

    S1 - 10 Connexion automatique après l'inscription

--- WebApp/php/AjouterUtilisateur.php
+++ WebApp/php/AjouterUtilisateur.php
@@ -4,12 +4,29 @@ require_once("Connexion.php");
 
 $log=$_POST['login'];
 $pass1=$_POST['pwd'];
-$pass2=$_POST['pwd2'];
 $email=$_POST['email'];
 
-    $pc=md5($pass1);
-    $req = "insert into UTILISATEUR (IDENTIFIANT,EMAIL, MOT_DE_PASSE) VALUE ('$log','$email','$pc')";
-    mysql_query($req) or die(mysql_error());
+$pc=md5($pass1);
+$req = "insert into UTILISATEUR (IDENTIFIANT,EMAIL, MOT_DE_PASSE) VALUE ('$log','$email','$pc')";
+mysql_query($req) or die(mysql_error());
+header("location:../index.html");
+
+$pc = md5($pass1);
+
+$request="select * from UTILISATEUR where IDENTIFIANT='$log' and MOT_DE_PASSE='$pc'";
+$result=mysql_query($request) or die (mysql_error());
+
+if($u=mysql_fetch_assoc($result))
+{
+    session_start();
+    $_SESSION['Niv'] = $u['ID'];
+    header("location:../liste-projets.html");
+
+}
+else{
+
     header("location:../index.html");
+}
+
 
 ?>
