fc3dcde52ad176a76d112bc86ba243c84239184a
yasbenzineb <yanssine93@gmail.com>
2015-11-25 10:50:36 +0100

    La suppression  d'un projet

--- WebApp/php/SupprimerProjet.php
+++ WebApp/php/SupprimerProjet.php
@@ -2,11 +2,12 @@
 
 require_once("Verifier1.php");
 require_once("Connexion.php");
+
 $id=$_GET['id'];
-$req="delete from projets WHERE (id=$id)";
+$req="delete from projet WHERE (id=$id)";
 mysql_query($req) or die(mysql_error());
 
-header("location:ListeProjets.php"); // Redirection
-//require_once("AfficherJoueur.php"); //Forward
+header("location:../liste-projets.html"); // Redirection
+
 
 ?>
