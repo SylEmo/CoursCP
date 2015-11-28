f5e7aed8fe71302e5d43545fe0b22f98a09051d7
Thomasgitt <thomas.gittinger@live.fr>
2015-11-28 15:48:37 +0100

    S4 - 2 Kanban

---
+++ WebApp/php/RemplirKanban.php
@@ -0,0 +1,18 @@
+<?php
+$idSprint = $_POST['id'];
+
+
+require_once("Verifier1.php");
+session_start();
+require_once("Connexion.php");
+
+$req= "SELECT * FROM TACHE WHERE IDSPRINT =(SELECT ID FROM SPRINT WHERE ID=".$idSprint." AND IDPROJET IN(SELECT ID FROM PROJET WHERE IDUTIL=".$_SESSION['Niv']."))";
+
+$rs=mysql_query($req) or die(mysql_error());
+
+$result='';
+while($pr=mysql_fetch_assoc($rs)) {
+	$result.="<li><h3 class=\"name\"><a>".$pr['DESCRIPTION']."</a></h3><a>".$pr['IDENTIFIANT']."</a> <a>".$pr['COUT']."</a></li>";
+}
+echo utf8_encode($result);
+?>
