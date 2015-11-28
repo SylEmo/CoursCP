c06d3d0f082dcbddcce0a21667df2a41abb08621
SylEmo <sylvain.emonet@orange.fr>
2015-11-28 23:46:06 +0100

    S0 - T0 : Dette technique : Ajout d'une fonction pour récupérer l'idProjet et idSprint si nécessaire dans l'URL

--- WebApp/js/adopi.js
+++ WebApp/js/adopi.js
@@ -5,10 +5,32 @@ function recupererIdProjetURL(){
 	
 	params = params.substring(1);
 	params = params.split("&");
-	for(i=0;i<params.length;i++){
+	for(i = 0; i < params.length; i++){
 		param = params[i].split("=");
 		if(param[0] == "id"){
 			return param[1];
 		}
 	}
+}
+
+function recupererIdsProjetSprintURL(){
+	var params = window.location.search;
+	var param;
+	var i;
+	var idProjet, idSprint;
+	var tabIds = [];
+
+	params = params.substring(1);
+	params = params.split("&");
+	for(i = 0; i < params.length; i++){
+		param = params[i].split("=");
+		if(param[0] == "idProjet"){
+			idProjet = param[1];
+		}
+		if(param[0] == "idSprint"){
+			idSprint = param[1];
+		}
+	}
+	tabIds.push({idProjet : idProjet, idSprint : idSprint});
+	return tabIds;
 }
