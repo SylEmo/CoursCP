cb4d0d0a1d7c77dfd810c0769634dbf72051d894
Thomasgitt <thomas.gittinger@live.fr>
2015-11-28 15:49:56 +0100

    S1 - Modification suppression projet

--- WebApp/php/SupprimerProjet.php
+++ WebApp/php/SupprimerProjet.php
@@ -4,7 +4,7 @@ require_once("Verifier1.php");
 require_once("Connexion.php");
 
 $id=$_GET['id'];
-$req="delete from projet WHERE (id=$id)";
+$req="delete from PROJET WHERE (ID=$id)";
 mysql_query($req) or die(mysql_error());
 
 header("location:../liste-projets.html"); // Redirection
