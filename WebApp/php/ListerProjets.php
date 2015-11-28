86705109f79def05f0c8d424f72e4ce465e7f81a
Thomasgitt <thomas.gittinger@live.fr>
2015-11-28 23:03:12 +0100

    S1 - 11 Modification du format de retour du php (pour tri et recherche)

--- WebApp/php/ListerProjets.php
+++ WebApp/php/ListerProjets.php
@@ -10,7 +10,10 @@ $rs=mysql_query($req) or die(mysql_error());
 
 $result='';
 while($pr=mysql_fetch_assoc($rs)) {
-	$result.="<li><h3 class=\"name\"><a onclick=\"ouvrirProjet(".$pr['ID'].");\">".$pr['NOM']."</a></h3><a onclick=\"modifierProjet(".$pr['ID'].");\">Modifier</a> <a onclick=\"supprimerProjet(".$pr['ID'].");\">Supprimer</a></li>";
+	//$result.="<li><h3 class=\"name\"><a onclick=\"ouvrirProjet(".$pr['ID'].");\">".$pr['NOM']."</a></h3><a onclick=\"modifierProjet(".$pr['ID'].");\">Modifier</a> <a onclick=\"supprimerProjet(".$pr['ID'].");\">Supprimer</a></li>";
+	$result.= $pr['ID'] .'\|/'.$pr['NOM'].'\|/';
 }
+//on efface le dernier "-" pour aider le split dans le fichier liste-projets.html et ainsi empecher d'avoir une entrée vie dans le tableau et donc une ligne de projet vide
+$result = substr($result, 0, -3);
 echo $result;
 ?>
