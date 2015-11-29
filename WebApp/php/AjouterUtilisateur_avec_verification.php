4955bfd7e4e3701ef394f1f5dbbf62ab33706372
yasbenzineb <yanssine93@gmail.com>
2015-11-29 20:21:29 +0100

    S0-Vérification avant l'inscription

---
+++ WebApp/php/AjouterUtilisateur_avec_verification.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once("Connexion.php");
+
+$log=$_POST['login'];
+$pass1=$_POST['pwd'];
+$email=$_POST['email'];
+
+$pc=md5($pass1);
+
+
+$sql=mysql_query("SELECT FROM UTILISATEUR (IDENTIFIANT, EMAIL, MOT_DE_PASSE) WHERE EMAIL=$email");
+if(mysql_num_rows($sql)>=1)
+{
+    echo " vous etes deja inscrit";
+    header("location:../index.html");
+}
+else
+{
+    $req = "insert into UTILISATEUR (IDENTIFIANT,EMAIL, MOT_DE_PASSE) VALUE ('$log','$email','$pc')";
+    mysql_query($req) or die(mysql_error());
+    $request="select * from UTILISATEUR where IDENTIFIANT='$log' and MOT_DE_PASSE='$pc'";
+    $result=mysql_query($request) or die (mysql_error());
+
+    if($u=mysql_fetch_assoc($result))
+    {
+        session_start();
+        $_SESSION['Niv'] = $u['ID'];
+        header("location:../liste-projets.html");
+
+    }
+    else{
+
+        header("location:../index.html");
+    }
+
+
+}
+
+?>
