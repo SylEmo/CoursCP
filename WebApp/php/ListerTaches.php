834240bd6b93b79bd2b2c7d39c5160219f8e503a
SylEmo <sylvain.emonet@orange.fr>
2015-11-30 00:29:10 +0100

    S2 - T14 : Mise à jour de la structure des données renvoyées pour la liste des tâches.

--- WebApp/php/ListerTaches.php
+++ WebApp/php/ListerTaches.php
@@ -14,7 +14,8 @@ $rs=mysql_query($req) or die(mysql_error());
 
 $result='';
 while($pr=mysql_fetch_assoc($rs)) {
-	$result.="<tr id=\"3\" style=\"min-height: 28px;\"><td class=\"number\">".$pr['ID']."</td><td>".$pr['IDENTIFIANT']."</td><td>".$pr['DESCRIPTION']."</td><td class=\"number\">".$pr['COUT']."</td><td><a onclick=\"editableGrid.removeRow(".$pr['ID'].");\">Supprimer</a></td></tr>";
+	// $result.="<tr id=\"3\" style=\"min-height: 28px;\"><td class=\"number\">".$pr['ID']."</td><td>".$pr['IDENTIFIANT']."</td><td>".$pr['DESCRIPTION']."</td><td class=\"number\">".$pr['COUT']."</td><td><a onclick=\"editableGrid.removeRow(".$pr['ID'].");\">Supprimer</a></td></tr>";
+	$result.= $pr['ID'] .'\|/'. $pr['IDENTIFIANT'] .'\|/'. $pr['DESCRIPTION'] .'\|/'. $pr['COUT'] .'\|/';
 }
 echo utf8_encode($result);
 ?>
