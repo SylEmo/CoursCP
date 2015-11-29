293917ea6e5c242ee1e5a9c11f0f7ac0a05c1e61
SylEmo <sylvain.emonet@orange.fr>
2015-11-29 02:02:40 +0100

    S2 - T11 : Modification de la structure des données envoyées par le PHP

--- WebApp/php/ListerUserStory.php
+++ WebApp/php/ListerUserStory.php
@@ -13,7 +13,8 @@ $rs=mysql_query($req) or die(mysql_error());
 
 $result='';
 while($pr=mysql_fetch_assoc($rs)) {
-	$result.="<tr id=\"3\" style=\"min-height: 28px;\"><td class=\"number\">".$pr['ID']."</td><td>".$pr['IDENTIFIANT']."</td><td>".$pr['DESCRIPTION']."</td><td class=\"number\">".$pr['PRIORITE']."</td><td class=\"number\">".$pr['DIFFICULTE']."</td><td><a onclick=\"editableGrid.removeRow(".$pr['ID'].");\">Supprimer</a></td></tr>";
+	//$result.="<tr id=\"3\" style=\"min-height: 28px;\"><td class=\"number\">".$pr['ID']."</td><td>".$pr['IDENTIFIANT']."</td><td>".$pr['DESCRIPTION']."</td><td class=\"number\">".$pr['PRIORITE']."</td><td class=\"number\">".$pr['DIFFICULTE']."</td><td><a onclick=\"editableGrid.removeRow(".$pr['ID'].");\">Supprimer</a></td></tr>";
+	$result.= $pr['ID'] .'\|/'. $pr['IDENTIFIANT'] .'\|/'. $pr['DESCRIPTION'] .'\|/'. $pr['PRIORITE'] .'\|/'. $pr['DIFFICULTE'] .'\|/';
 }
 echo $result;
 ?>
