6789fb3e1239ef4237a7fe8aa3c301cc2a5f5b57
Thomasgitt <thomas.gittinger@live.fr>
2015-11-30 11:31:28 +0100

    S0 - T0 Dette technique : verification avant l'inscription du mail ET du pseudo

--- WebApp/php/AjouterUtilisateur.php
+++ WebApp/php/AjouterUtilisateur.php
@@ -7,25 +7,34 @@ $pass1=$_POST['pwd'];
 $email=$_POST['email'];
 
 $pc=md5($pass1);
-$req = "insert into UTILISATEUR (IDENTIFIANT,EMAIL, MOT_DE_PASSE) VALUE ('$log','$email','$pc')";
-mysql_query($req) or die(mysql_error());
-header("location:../index.html");
 
-$pc = md5($pass1);
 
-$request="select * from UTILISATEUR where IDENTIFIANT='$log' and MOT_DE_PASSE='$pc'";
-$result=mysql_query($request) or die (mysql_error());
-
-if($u=mysql_fetch_assoc($result))
+$sql=mysql_query("SELECT * FROM UTILISATEUR WHERE EMAIL='".$email."' OR IDENTIFIANT='".$log."'") or die(mysql_error());
+if(mysql_num_rows($sql)>=1)
 {
-    session_start();
-    $_SESSION['Niv'] = $u['ID'];
-    header("location:../liste-projets.html");
-
-}
-else{
-
+    echo " vous etes deja inscrit";
     header("location:../index.html");
+}
+else
+{
+	$req = "insert into UTILISATEUR (IDENTIFIANT,EMAIL, MOT_DE_PASSE) VALUE ('".$log."','".$email."','".$pc."')";
+	mysql_query($req) or die(mysql_error());
+	header("location:../index.html");
+
+	$request="select * from UTILISATEUR where IDENTIFIANT='$log' and MOT_DE_PASSE='$pc'";
+	$result=mysql_query($request) or die (mysql_error());
+
+	if($u=mysql_fetch_assoc($result))
+	{
+	    session_start();
+	    $_SESSION['Niv'] = $u['ID'];
+	    header("location:../liste-projets.html");
+
+	}
+	else{
+
+	    header("location:../index.html");
+	}
 }
 
 
